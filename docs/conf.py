874f61ee14f014c35b0f2e9b6ea5772a4fac81b7
gonzalday <raquelgonzalezalday@gmail.com>
2023-05-24 12:42:14 +0200

    Hide title form navbar docs

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_logo = "_static/logo_fixed_white.svg"
 html_favicon = "_static/favicon_resomapper.ico"
-# html_theme_options = {
-#     "logo_only": True,
-#     "display_version": False,
-# }
+html_theme_options = {
+    "logo_only": True,
+    "display_version": True,
+}
