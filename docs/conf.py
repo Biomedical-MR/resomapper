305f199d4758fabb9ab6e24f7ad52c12c06256e3
gonzalday <raquelgonzalezalday@gmail.com>
2023-05-24 12:10:13 +0200

    Show logo and hide version in docs

--- docs/conf.py
+++ docs/conf.py
@@ -47,3 +47,7 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static/"]
 html_logo = "logo_fixed_white.svg"
 html_favicon = "favicon_resomapper.ico"
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+}
